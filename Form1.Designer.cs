0ea3d064e77c04e6348035b4da13e658b8209722
Marcin <Marcin@MARCIN-PC>
2017-06-14 23:36:47 +0200

    zxkcn

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,8 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -47,6 +49,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button10_Click);
             // 
             // button4
             // 
@@ -56,6 +59,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button10_Click);
             // 
             // button5
             // 
@@ -65,6 +69,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button10_Click);
             // 
             // button2
             // 
@@ -74,6 +79,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button10_Click);
             // 
             // button3
             // 
@@ -83,6 +89,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button10_Click);
             // 
             // button6
             // 
@@ -92,6 +99,7 @@
             this.button6.TabIndex = 7;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button10_Click);
             // 
             // button7
             // 
@@ -101,6 +109,7 @@
             this.button7.TabIndex = 8;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button10_Click);
             // 
             // button8
             // 
@@ -110,34 +119,44 @@
             this.button8.TabIndex = 9;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button10_Click);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(100, 307);
+            this.button10.Location = new System.Drawing.Point(100, 300);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(100, 23);
+            this.button10.Size = new System.Drawing.Size(100, 35);
             this.button10.TabIndex = 11;
             this.button10.Text = "Start";
             this.button10.UseVisualStyleBackColor = true;
             this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 342);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button10);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -153,6 +172,7 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,30 +7,139 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
+
 
 namespace puzzle
 {
     public partial class Form1 : Form
     {
-        Point Point1;
-       
+        Point Point;
+        ArrayList images = new ArrayList();
         public Form1()
         {
-            Point1.X = 180;
-            Point1.Y = 180;
+            Point.X = 180;
+            Point.Y = 180;
 
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        private void cropImageTomages(Image orginal, int w, int h)
         {
-            
+            Bitmap bmp = new Bitmap(w, h);
+
+            Graphics graphic = Graphics.FromImage(bmp);
+
+            graphic.DrawImage(orginal, 0, 0, w, h);
+
+            graphic.Dispose();
+
+            int movr = 0, movd = 0;
+
+            for (int x = 0; x < 8; x++)
+            {
+                Bitmap piece = new Bitmap(90, 90);
+
+                for (int i = 0; i < 90; i++)
+                    for (int j = 0; j < 90; j++)
+                        piece.SetPixel(i, j,
+                            bmp.GetPixel(i + movr, j + movd));
+
+                images.Add(piece);
+
+                movr += 90;
+
+                if (movr == 300)
+                {
+                    movr = 0;
+                    movd += 90;
+                }
+            }
 
         }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in panel1.Controls)
+                b.Enabled = true;
+
+            Image orginal = Image.FromFile(@"C:\Users\Marcin\Desktop\das");
+
+            cropImageTomages(orginal, 300, 300);
+
+            AddImagesToButtons(images);
+
+        }
+        private void AddImagesToButtons(ArrayList images)
+        {
+            int i = 0;
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+
+            arr = suffle(arr);
+
+            foreach (Button b in panel1.Controls)
+            {
+                if (i < arr.Length)
+                {
+                    b.Image = (Image)images[arr[i]];
+                    i++;
+                }
+            }
+
+
+        }
+        private int[] suffle(int[] arr)
+        {
+            Random rand = new Random();
+            arr = arr.OrderBy(x => rand.Next()).ToArray();
+
+            return arr;
+        }
+        private int[] suffle(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
     }
+       private void button9_Click(object sender, EventArgs e)
+    {
+        MoveButton((Button)sender);
+    }
+
+    private void MoveButton(Button btn)
+    {
+        if (((btn.Location.X == Point.X - 90 || btn.Location.X == Point.X + 90)
+            && btn.Location.Y == Point.Y)
+            || (btn.Location.Y == Point.Y - 90 || btn.Location.Y == Point.Y + 90)
+            && btn.Location.X == Point.X)
+        {
+            Point swap = btn.Location;
+            btn.Location = Point;
+            Point = swap;
+        }
+
+        if (Point.X == 180 && Point.Y == 180)
+            CheckValid();
+    }
+
+    private void CheckValid()
+    {
+        int count = 0, index;
+        foreach (Button btn in panel1.Controls)
+        {
+            index = (btn.Location.Y / 90) * 3 + btn.Location.X / 90;
+            if (images[index] == btn.Image)
+                count++;
+        }
+        if (count == 8)
+            MessageBox.Show("well done you win!");
+    }
+
+
+
 }
