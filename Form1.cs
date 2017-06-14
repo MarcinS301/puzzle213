3319e343b6efddc6041fd1d1c8ba09fd68093f90
Marcin <Marcin@MARCIN-PC>
2017-06-14 23:19:15 +0200

    Przesuwanie się przycisków oraz zdjęcie... niedziałające

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle
+{
+    public partial class Form1 : Form
+    {
+        Point Point1;
+       
+        public Form1()
+        {
+            Point1.X = 180;
+            Point1.Y = 180;
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
